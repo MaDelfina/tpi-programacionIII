da4f1c350a3555b0f3da665a33ca3737aa71750e
N-Rem <nicolasromero.barrios@gmail.com>
2024-06-18 19:32:18 -0300

    Add Infraestructure

---
+++ Interface/Data/RepositoryUser.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Interface;
+
+namespace Infrastructure.Data
+{
+    public class RepositoryUser : RepositoryBase<User>, IRepositoryUser
+    {
+        private readonly ApplicationContext _context;
+        public RepositoryUser(ApplicationContext context) : base(context)
+        {
+            _context = context;
+        }
+
+
+    }
+}
